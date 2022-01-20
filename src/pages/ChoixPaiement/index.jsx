f74ac3a6bde06b6bc7b08355c0896d43a554999a
[franck59840] <[depoorter.franck@gmail.com]>
2022-01-20 15:34:22 +0100

    Create page ChoixPaiement and route

---
+++ src/pages/ChoixPaiement/index.jsx
@@ -0,0 +1,11 @@
+import Paiement from '../../components/ListPaiement';
+import Header from '../../components/Header';
+
+export default function Crypto() {
+  return (
+    <div>
+      <Header />
+      <Paiement />
+    </div>
+  );
+}
