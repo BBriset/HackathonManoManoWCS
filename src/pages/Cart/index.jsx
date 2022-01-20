343da2330bbc5516e581eb6580e51b9dd069e096
BBriset <briset.b@gmail.com>
2022-01-20 12:31:40 +0100

    Cart page front static items

--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -10,33 +10,45 @@ import TabPanel from '@mui/lab/TabPanel';
 import hammer from 'assets/hammer.png';
 import paint from 'assets/paint.png';
 import wall from 'assets/wall.png';
+import monPanier from 'assets/monPanier.png';
+import tronconneuse from 'assets/tronconneuse.jpg';
+import tv from 'assets/tv.jpg';
 import SCart from './style';
 
 export default function Cart() {
   const BlackAl = styled(AngleLeft)`
     color: black;
     width: 14px !important;
+    margin-right: 10px;
   `;
+
   const [value, setValue] = useState('1');
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
+  const price1 = 116.9;
+  const price2 = 89.99;
+  const total = price1 + price2;
+
   return (
     <SCart>
       <TabContext value={value}>
         <Box>
           <div>
+            <Link className="link" to="/">
+              <BlackAl />
+              Continuer mes achats
+            </Link>
             <Tabs
               variant="fullWidth"
               onChange={handleChange}
-              aria-label="ConnectionChoice"
+              aria-label="CartChoise"
               textColor="black"
               centered
               TabIndicatorProps={{
                 style: {
-                  height: '3px',
+                  height: '5px',
                   backgroundColor: 'black',
-                  width: '27vw',
                 },
               }}
             >
@@ -45,50 +57,69 @@ export default function Cart() {
             </Tabs>
           </div>
         </Box>
-        <TabPanel value="1" />
-        <TabPanel value="2" />
-      </TabContext>
-
-      <div className="layoutPagecontainer">
-        <div className="layoutLayout">
-          <article className="layoutLeftColumn">
-            <div className="cartContent">
-              <nav>
-                <Link to="/">
-                  <BlackAl />
-                  Continuer mes achats
-                </Link>
-              </nav>
+        <TabPanel value="1">
+          <h2>Total {total} €</h2>
+          <article className="item">
+            <img src={tronconneuse} alt="meuble tv" />
+            <div className="description">
+              <p className="itemDetails">
+                Tronçonneuse thermique 62 cm3, guide 50 cm, 2 chaines + housse
+                de transport
+              </p>
+              {price1}€
             </div>
           </article>
-        </div>
-      </div>
+          <article className="item">
+            <img src={tv} alt="meuble tv" />
+            <div className="description">
+              <p className="itemDetails">
+                Selsey BIANKO - Meuble TV / Banc TV (chêne lancaster, 140 cm,
+                sans LED)
+              </p>
+              {price2} €
+            </div>
+          </article>
+          <h2>Total {total} €</h2>
+          <Link className="buttonLink" to="/">
+            Continuer mes achats
+          </Link>
+          <Link className="buttonLink" to="/buy">
+            Acheter ces articles
+          </Link>
+          <Link className="buttonLink" to="/connexion">
+            Connexion/Inscription
+          </Link>
+        </TabPanel>
+        <TabPanel value="2">
+          <h1>Ça alors. Vous n’avez rien mis de côté.</h1>
+          <p>On a vérifié partout.</p>
+          <img src={monPanier} alt="Mon panier" />
+        </TabPanel>
+      </TabContext>
 
-      <div>tab</div>
       <div className="reassuranceSection">
         <h2 className="reassuranceTitle">
-          Le plus grand magasin en ligne d`&lsquo;`Europe de brico, maison &
-          jardin
+          Le plus grand magasin en ligne d’Europe de brico, maison & jardin
         </h2>
         <ul className="reassuranceList">
           <li className="reassuranceItem">
             <img src={hammer} alt="hammer" />
             <div className="reassuranceDescription">
               <h3>Des millions de produits</h3>
-              <p>62 fois plus qu`&lsquo;`en magasin, ça en fait du choix</p>
+              <p>62 fois plus qu’en magasin, ça en fait du choix</p>
             </div>
           </li>
           <li className="reassuranceItem">
             <img src={wall} alt="wall" />
             <div className="reassuranceDescription">
               <h3>Livraison chez vous ou en point relais</h3>
-              <p>Ça, c`&lsquo;`est vous qui voyez</p>
+              <p>Ça, c’est vous qui voyez</p>
             </div>
           </li>
           <li className="reassuranceItem">
             <img src={paint} alt="hammer" />
             <div className="reassuranceDescription">
-              <h3>Des centaines d`&lsquo;`experts en ligne</h3>
+              <h3>Des centaines d’experts en ligne</h3>
               <p>Aussi experts que Macgyver, plus dispos que votre ophtalmo</p>
             </div>
           </li>
--- src/pages/Cart/style.jsx
+++ src/pages/Cart/style.jsx
@@ -1,9 +1,68 @@
 import styled from 'styled-components';
 
 const SCart = styled.section`
+  .link {
+    display: flex;
+    text-decoration: none;
+    color: black;
+    align-items: center;
+    margin-left: 10px;
+    margin-top: 2vh;
+    font-weight: bolder;
+  }
+  .buttonLink {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 8vh;
+    width: 90vw;
+    background: #53baad;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 10px;
+    text-decoration: none;
+    margin-bottom: 4vh;
+    color: black;
+  }
+  .item {
+    display: flex;
+    height: 15vh;
+    width: 90vw;
+    margin-bottom: 4vh;
+    background: #505971;
+    box-shadow: 0px 4px 4px #53baad;
+    border-radius: 10px;
+    padding: 10px;
+    color: white;
+    justify-content: space-around;
+    font-weight: bold;
+    font-size: 12px;
+    img {
+      width: 33vw;
+    }
+    .description {
+      width: 50vw;
+      padding: 5px;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+      .itemDetails {
+        margin: 0;
+        height: 6vh;
+      }
+    }
+  }
+  .MuiTabPanel-root {
+    display: flex;
+    width: 100vw;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: none;
+  }
+
   .reassuranceSection {
     max-width: 1192px;
-    margin-left: auto;
+    margin-left: 2vw;
     margin-right: auto;
   }
 
@@ -21,22 +80,22 @@ const SCart = styled.section`
   }
   .reassuranceDescription {
     margin-left: 16px;
-    h3{
+    h3 {
       margin-top: 8px;
-  margin-bottom: 0;
-  font-size: 16px;
-  line-height: 24px;
-  font-weight: 600;
-  color: #0c193a;
-      }
-    p{
+      margin-bottom: 0;
+      font-size: 16px;
+      line-height: 24px;
+      font-weight: 600;
+      color: #0c193a;
+    }
+    p {
       margin-top: 0;
       margin-bottom: 8px;
       font-size: 16px;
       line-height: 24px;
       font-weight: 400;
-      color: #0c193a;}
-  }
+      color: #0c193a;
+    }
   }
   .reassuranceItem {
     margin-bottom: 12px;
