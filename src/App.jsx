474a841a46c1378ce1fc84a44c95b3696d044ed6
BBriset <briset.b@gmail.com>
2022-01-21 10:40:26 +0100

    All is all

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from 'pages/Home';
-import Form from './pages/Form';
 
 import Cart from 'pages/Cart';
 import Header from 'components/Header';
 import Footer from 'components/Footer';
+import Form from './pages/Form';
 import Article from './pages/Article';
 import Choix from './pages/ChoixPaiement';
 import Inscription from './pages/Inscription';
@@ -22,8 +22,8 @@ export default function App() {
         <Route path="/Paiement" element={<Choix />} />
         <Route path="/Connexion" element={<Connexion />} />
         <Route path="/Inscription" element={<Inscription />} />
-
       </Routes>
+      <Footer />
     </>
   );
 }
--- src/pages/Connexion/index.jsx
+++ src/pages/Connexion/index.jsx
@@ -5,32 +5,38 @@ import SFromInscript from './style';
 export default function Conne() {
   return (
     <SFromInscript>
-      <h1>Connexion</h1>
-      <div className="div1">
-        <h2>Mail</h2>
-        <input type="text" className="Mail" placeholder="Username" />
-      </div>
-      <div className="div2">
-        <h2>Mot de passe</h2>
+      <div className="conn">
+        <h1>Connexion</h1>
+        <form>
+          <div className="div1">
+            <h2>Mail</h2>
+            <input type="text" className="Mail" placeholder="Username" />
+          </div>
+          <div className="div2">
+            <h2>Mot de passe</h2>
 
-        <input type="text" className="m-d-p" placeholder="Password" />
-      </div>
-      <Link type="button" className="buttonLink" to="/Connexion">
-        Connexion
-      </Link>
-      <div className="div3">
-        <div>
-          <input type="checkbox" className="remember" /> Remember ME
-        </div>
-        <div>
-          <a href="/">Forgot your password</a>
+            <input type="text" className="m-d-p" placeholder="Password" />
+          </div>
+        </form>
+        <Link type="button" className="buttonLink" to="/Connexion">
+          Connexion
+        </Link>
+        <div className="div3">
+          <div>
+            <input type="checkbox" className="remember" /> Remember ME
+          </div>
+          <div>
+            <a href="/">Forgot your password</a>
+          </div>
         </div>
       </div>
       <div>
-        <h1 className="inscription"> pas de compte ?</h1>
-        <Link type="button" className="buttonLink" to="/Inscription">
-          Inscrivez-vous
-        </Link>
+        <div className="ins">
+          <h1 className="inscription"> Pas de compte ?</h1>
+          <Link type="button" className="buttonLink" to="/Inscription">
+            Inscrivez-vous
+          </Link>
+        </div>
       </div>
     </SFromInscript>
   );
