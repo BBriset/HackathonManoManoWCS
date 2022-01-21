bac02358e195d5f8f3ad05ea5d5b80c614838ab9
[franck59840] <[depoorter.franck@gmail.com]>
2022-01-21 02:01:31 +0100

    refactoring

--- src/pages/Form/index.jsx
+++ src/pages/Form/index.jsx
@@ -1,44 +1,38 @@
-import tronconneuse from './tronconneuse.jpg';
-import { SForm, SLivraison } from './style';
+import tronconneuse from 'assets/tronconneuse.jpg';
+import { SForm, STitle, SCoordinate, SLivraison, SButton } from './style';
 
 export default function Form() {
   return (
     <>
       <SForm>
-        <article>
-          <div className="direct">
-            <h1>Achat Direct</h1>
-            <p>Tronçonneuse Thermique</p>
-            <img src={tronconneuse} alt="tronçonneuse" />
-          </div>
-        </article>
-        <article>
-          <div className="form">
-            <div className="title">
-              <h1>Coordonnées</h1>
-            </div>
-            <h2>Nom</h2>
-            <input type="text" id="first-name" />
-            <h2>Prénom</h2>
-            <input type="text" name="prénom" />
-            <h2>Adresse</h2>
-            <input type="textarea" name="adresse" />
-            <h2>Code Postal</h2>
-            <input type="text" name="code Postal" />
-            <h2>Ville </h2>
-            <input type="text" name="ville" />
-            <h2> E-mail</h2>
-            <input type="email" id="email" name="email" />
-          </div>
-        </article>
-        <SLivraison>
-          <div className="chooseDelivery" />
+        <STitle>
+          <h1>Achat Direct</h1>
+          <p>Tronçonneuse Thermique</p>
+          <img src={tronconneuse} alt="tronçonneuse" />
+        </STitle>
+        <SCoordinate>
+          <hr />
           <div className="title">
-            <h1>Choix de la Livraison</h1>
+            <h2>Coordonnées</h2>
           </div>
-          <div className="address">
-            <h2>Adresse</h2>
-          </div>
+
+          <input type="text" id="first-name" placeholder="Nom" />
+
+          <input type="text" name="prénom" placeholder="Prénom" />
+
+          <input type="textarea" name="adresse" placeholder="Adresse" />
+
+          <input type="text" name="code Postal" placeholder="Code Postal" />
+
+          <input type="text" name="ville" placeholder="Ville" />
+
+          <input type="email" id="email" name="email" placeholder="Email" />
+        </SCoordinate>
+        <SLivraison>
+          <hr />
+          <h2>Choix de la Livraison</h2>
+          <h3>Adresse</h3>
+
           <div className="relay">
             <h2>Points Relais autour de chez vous</h2>
             <p>Point Relais</p>
@@ -46,9 +40,9 @@ export default function Form() {
             <p>Point Relais</p>
           </div>
         </SLivraison>
-        <button type="button" className="paymentBtn">
-          Payer
-        </button>
+        <SButton>
+          <button type="button">Payer</button>
+        </SButton>
       </SForm>
     </>
   );
