2685164fd266294ecc4e63087dc3f90d0b66031d
[franck59840] <[depoorter.franck@gmail.com]>
2022-01-21 10:26:23 +0100

    mise en page du form

--- src/pages/Form/index.jsx
+++ src/pages/Form/index.jsx
@@ -1,44 +1,57 @@
-import tronconneuse from 'assets/tronconneuse.jpg';
-import { SForm, STitle, SCoordinate, SLivraison, SButton } from './style';
+import VroumVroum from 'assets/tronconneuse.jpg';
+import {
+  SForm,
+  STitle,
+  SCoordinate,
+  SLivraison,
+  SButton,
+  PointRelaisA,
+  ListImput,
+} from './style';
 
 export default function Form() {
   return (
     <>
       <SForm>
+        <h1>Achat Direct</h1>
         <STitle>
-          <h1>Achat Direct</h1>
+          <img src={VroumVroum} alt="tronçonneuse" />
           <p>Tronçonneuse Thermique</p>
-          <img src={tronconneuse} alt="tronçonneuse" />
         </STitle>
         <SCoordinate>
           <hr />
           <div className="title">
             <h2>Coordonnées</h2>
           </div>
-
-          <input type="text" id="first-name" placeholder="Nom" />
-
-          <input type="text" name="prénom" placeholder="Prénom" />
-
-          <input type="textarea" name="adresse" placeholder="Adresse" />
-
-          <input type="text" name="code Postal" placeholder="Code Postal" />
-
-          <input type="text" name="ville" placeholder="Ville" />
-
-          <input type="email" id="email" name="email" placeholder="Email" />
+          <ListImput>
+            <input type="text" id="first-name" placeholder="Nom" />
+            <input type="text" name="prénom" placeholder="Prénom" />
+            <input type="textarea" name="adresse" placeholder="Adresse" />
+            <input type="text" name="code Postal" placeholder="Code Postal" />
+            <input type="text" name="ville" placeholder="Ville" />
+            <input type="email" id="email" name="email" placeholder="Email" />
+          </ListImput>
         </SCoordinate>
         <SLivraison>
           <hr />
           <h2>Choix de la Livraison</h2>
           <h3>Adresse</h3>
-
-          <div className="relay">
-            <h2>Points Relais autour de chez vous</h2>
-            <p>Point Relais</p>
-            <p>Point Relais</p>
-            <p>Point Relais</p>
-          </div>
+          <p>4 Av. des Saules bâtiment Le Doge, 59160 Lille</p>
+          <h3>Points Relais autour de chez vous</h3>
+          <PointRelaisA>
+            <label htmlFor="Point relais 1">
+              <input type="radio" id="point relais 1" name="point relais 1" />
+              Point relais 1
+            </label>
+            <label htmlFor="Point relais 2">
+              <input type="radio" id="point relais 2" name="point relais 2" />
+              Point relais 2
+            </label>
+            <label htmlFor="Point relais 3">
+              <input type="radio" id="point relais 3" name="point relais 3" />
+              Point relais 3
+            </label>
+          </PointRelaisA>
         </SLivraison>
         <SButton>
           <button type="button">Payer</button>
